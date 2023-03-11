04cf1fcab2243e955784f65c465c8ef53cf27b97
Harsh Shah <harshvshah.22@gmail.com>
2023-03-11 12:04:02 -0500

    fifth commit

--- utils/get_text.py
+++ utils/get_text.py
@@ -1,22 +1,16 @@
 # import libraries
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.firefox import GeckoDriverManager
 import streamlit as st
 
 
 # get text from the webpage and dump it into a json variable
 @st.cache_data
 def get_text(URL):
-    firefoxOptions = Options()
-    firefoxOptions.add_argument("--headless")
-    service = Service(GeckoDriverManager().install())
-    driver = webdriver.Firefox(options=firefoxOptions,service=service,)
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
     driver.get(URL)
     elem = driver.find_element(By.TAG_NAME, "body")
     text = elem.text
